54a18072f54dfda7525e775ec981db9b6a27432f
BhoomiTechApps <bhoomitechfoundation@gmail.com>
2026-02-21 00:18:42 +0530

    Update ime.js

--- bmlt/modules/ime/ime.js
+++ bmlt/modules/ime/ime.js
@@ -1,1 +1,10 @@
+// modules/ime/ime.js
+import { transliterate } from "./transliterator.js";
+export function attachPhoneticIME(textarea,onUpdate){
+textarea.addEventListener("input",()=>{
+const buffer=textarea.value;
+const assamese=transliterate(buffer);
+if(onUpdate)onUpdate(buffer,assamese);
+});
+}
 
