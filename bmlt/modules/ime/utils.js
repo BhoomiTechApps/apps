6a7caabe87b3a6b5479340ee14a6fd656e353bb5
BhoomiTechApps <bhoomitechfoundation@gmail.com>
2026-02-24 18:25:40 +0530

    Delete bmlt directory

--- bmlt/modules/ime/utils.js
+++
@@ -1,9 +0,0 @@
-export function phoneticTransform(text) {
-  if (!text) return "";
-  let result = text;
-  result = result.replace(/O/g, "PROTECT_O"); 
-  result = result.replace(/yo\b/g, "oy"); 
-  result = result.replace(/PROTECT_O/g, "o"); 
-  return result.toLowerCase().trim();
-  result = result.replace(/্+o/g, "");
-}
