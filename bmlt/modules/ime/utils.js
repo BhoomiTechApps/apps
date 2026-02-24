46a4bd580a50ab906601afeb5a3e65c1edc86868
BhoomiTechApps <bhoomitechfoundation@gmail.com>
2026-02-24 17:50:10 +0530

    Update utils.js

--- bmlt/modules/ime/utils.js
+++ bmlt/modules/ime/utils.js
@@ -1,13 +1,9 @@
 export function phoneticTransform(text) {
   if (!text) return "";
-  
   let result = text;
-
   result = result.replace(/O/g, "PROTECT_O"); 
-
   result = result.replace(/yo\b/g, "oy"); 
-
   result = result.replace(/PROTECT_O/g, "o"); 
-  
   return result.toLowerCase().trim();
+  result = result.replace(/্+o/g, "");
 }
